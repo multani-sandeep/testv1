4dde0b3d1352ac0fbd9e05f861dfb3f9c06f5a04
sandeepsingh <s_multani@yahoo.com>
2016-05-11 14:24:09 +0100

    added geotools

--- hello-shop/src/main/java/hello/ShopsController.java
+++ hello-shop/src/main/java/hello/ShopsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello-world")
+@RequestMapping("/shops")
 public class ShopsController {
 
     private static final String template = "Hello, %s!";
